0bfb67198bd996438656eccbb4aa33c245ad3418
yrikiriki <toyota.30524@gmail.com>
2022-05-15 18:31:24 +0900

    修正１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit]
-  before_action :move_to_index, only: :edit
+  before_action :authenticate_user!, only: [:new, :edit,]
+  before_action :move_to_index, only: [:edit, :destroy]
   before_action :params_id, only: [:show, :edit, :update, :destroy]
 
   def index
