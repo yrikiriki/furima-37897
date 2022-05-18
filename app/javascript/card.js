631c78d99ca823dcea6e677e147d376eca8b5c58
yrikiriki <toyota.30524@gmail.com>
2022-05-18 10:10:16 +0900

    12

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -20,7 +20,6 @@ submit.addEventListener("click", (e) => {
       const renderDom = document.getElementById("charge-form");
       const tokenObj = `<input value=${token} name='token' type="hidden">`;
       renderDom.insertAdjacentHTML("beforeend",tokenObj);
-      debugger;
     }
 
     document.getElementById("card-number").removeAttribute("name");
